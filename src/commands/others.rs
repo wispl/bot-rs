f728c2b6ab284c6ef0f273ce815f7f3f852daf1d
wispl <wispl.8qbkk@slmail.me>
2024-11-22 01:37:05 -0500

    feat: define command

--- src/commands/others.rs
+++ src/commands/others.rs
@@ -1,6 +1,9 @@
-use crate::{Context, Data, Error};
+use poise::{serenity_prelude as serenity, CreateReply};
+use serde::Deserialize;
 
-use poise::CreateReply;
+use crate::{Context, Command, Error, traits::ContextExt};
+
+use tracing::{error, warn};
 
 #[poise::command(slash_command, category = "Others")]
 pub async fn ping(ctx: Context<'_>) -> Result<(), Error> {
@@ -24,6 +27,73 @@ pub async fn uptime(ctx: Context<'_>) -> Result<(), Error> {
     Ok(())
 }
 
-pub fn commands() -> [poise::Command<Data, Error>; 2] {
-    [ping(), uptime()]
+#[derive(Debug, Deserialize)]
+struct DictionaryResult {
+    #[allow(dead_code)]
+    word: String,
+    phonetics: String,
+    origin: String,
+
+    meanings: Vec<Meaning>,
+}
+
+#[derive(Debug, Deserialize)]
+struct Meaning {
+    #[serde(rename = "camelCase")]
+    part_of_speech: String,
+    definitions: Vec<Definition>,
+}
+
+#[derive(Debug, Deserialize)]
+struct Definition {
+    definition: String,
+    example: String,
+    #[allow(dead_code)]
+    synonyms: Vec<String>,
+    #[allow(dead_code)]
+    antonyms: Vec<String>,
+}
+
+#[poise::command(slash_command, category = "Others")]
+pub async fn define(
+    ctx: Context<'_>,
+    #[description = "word to define"] word: String,
+) -> Result<(), Error> {
+    ctx.defer_ephemeral().await?;
+    let reqwest = ctx.data().reqwest.clone();
+    let url = format!("https://api.dictionaryapi.dev/api/v2/entries/en/{}", word);
+    let json = reqwest
+        .get(url)
+        .send()
+        .await?
+        .json::<DictionaryResult>()
+        .await;
+    if let Ok(res) = json {
+        let embed = serenity::CreateEmbed::default()
+            .title(word)
+            .field("origin", &res.origin, false)
+            .field("phonetics", &res.phonetics, false)
+            .fields(res.meanings.iter().take(3).map(|m| {
+                (
+                    &m.part_of_speech,
+                    m.definitions
+                        .iter()
+                        .take(3)
+                        .map(|d| format!("```•\n{}\n\t{}\n```", d.definition, d.example))
+                        .collect::<Vec<String>>()
+                        .join(""),
+                    false
+                )
+            }));
+        ctx.send(poise::CreateReply::default().embed(embed)).await?;
+    } else {
+        ctx.say_ephemeral(format!("No definition found for {}", word))
+            .await?;
+    }
+
+    Ok(())
+}
+
+pub fn commands() -> [Command; 3] {
+    [ping(), uptime(), define()]
 }
